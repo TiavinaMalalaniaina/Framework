2d600a7a7be4b25044703ce47caed16341148e99
Tiavina Malalaniaina <tiavinaramia@gmail.com>
2023-06-12 00:01:56 +0300

    Add a function to cast and another function to get all setter of a class

--- Framework/src/java/etu2025/framework/util/Utils.java
+++ Framework/src/java/etu2025/framework/util/Utils.java
@@ -7,10 +7,13 @@ package etu2025.framework.util;
 import etu2025.framework.Mapping;
 import etu2025.framework.annotation.url;
 import java.io.File;
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -43,6 +46,75 @@ public class Utils {
             }
         }
         return mappingUrls;
+    }
+    
+    public static HashMap<String, Method> getAllSetters(Class<?> c) throws NoSuchMethodException {
+        HashMap<String, Method> setters = new HashMap<>();
+        for (Field f : c.getDeclaredFields()) {
+            setters.put(f.getName(), getSetter(c, f));
+        }
+        return setters;
+    }
+    
+    public static Method getSetter(Class<?> c, Field f) throws NoSuchMethodException {
+        String setter_query = getSetterQuery(f);
+        Method setter = c.getDeclaredMethod(setter_query, f.getType());
+        return setter;
+    }
+    
+    public static String getSetterQuery(Field f) {
+        String field_name = f.getName();
+        String t1 = field_name.substring(0, 1);
+        String t2 = field_name.substring(1);
+        t1 = t1.toUpperCase();
+        return "set" + t1 + t2;
+    }
+    
+    public static <T> T cast(String init, Class<?> type) throws Exception {
+        if (type.isArray()) {
+            throw new Exception("String can't be cast to be an array");
+        }
+        T result = null;
+        try {
+            if(type == Date.class) {
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                result = (T) format.parse(init);
+            } else if (type == Integer.class) {
+                result = (T) Integer.valueOf(init);
+            } else if (type == Float.class) {
+                result = (T) Float.valueOf(init);
+            } else if (type == Double.class) {
+                result = (T) Double.valueOf(init);
+            } else {
+                result = (T) init;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    
+    public static <T> T cast(String[] init, Class<?> type) throws Exception {
+        T result = null;
+        if (type.isArray()) {
+            Class<?> type_array = type.getComponentType();
+            if (init==null) {
+                result = (T) Array.newInstance(type_array, 0);
+            } else {
+                Object[] result_array = (Object[]) Array.newInstance(type_array, init.length);
+                for (int i=0; i<init.length; i++) {
+                    result_array[i] = cast(init[i], type_array);
+                }
+                result = (T) result_array;
+            }
+        } else {
+            if (init==null) {
+                result = null;
+            } else {
+                result = (T) cast(init[0], type);
+            }
+        }
+        return result;
     }
     
     

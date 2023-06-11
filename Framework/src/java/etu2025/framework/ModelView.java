90b3605a52cb8033675abe68da75e2b324dc5201
Tiavina Malalaniaina <tiavinaramia@gmail.com>
2023-06-11 22:32:54 +0300

    Create ModelView.java

---
+++ Framework/src/java/etu2025/framework/ModelView.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu2025.framework;
+
+/**
+ *
+ * @author tiavi
+ */
+public class ModelView {
+    private String view;
+
+    public ModelView(String view) {
+        setView(view);
+    }
+    public ModelView() {
+        
+    }
+    
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+    
+    
+}
